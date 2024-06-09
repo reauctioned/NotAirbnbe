aefa25ddb6a44731210c1877b178063987a0c2eb
Saurav <neechanzy@gmail.com>
2024-06-09 14:47:56 +0545

    save 1

---
+++ my-app/src/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Body(){
+    return(
+        <div className="body">
+        <img className="body--img1" src="image.png" alt=""/>
+        <img className="body--img6" src="buncha.png" />
+        <h1>Online Experiences</h1>
+        <p>Random text here : this is not offical airbnb </p>
+        </div>
+        )
+}
---
+++ my-app/src/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar(){
+    return(
+        <nav className='Nav'>
+        <img  className="Nav--logo" src='Airbnb_Logo.png' alt='bnb logo' />
+        </nav>
+    )
+}
