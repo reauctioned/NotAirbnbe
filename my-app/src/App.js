c1a15e4bf48fd6a18b96de6f59d9735385b475ae
Saurav <neechanzy@gmail.com>
2024-06-09 15:10:56 +0545

    airbnb

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './Navbar';
 import Body from './Body';
+import Card from './Card1';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
        <Body />
+       <Card />
     </div>
   );
 }
---
+++ my-app/src/Card1.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Card(){
+    return(
+        <div className="card">
+        <img src="card1.png" alt='card1'/>
+          <div className="star">
+               <img className="star" src="star.png" />
+          </div>
+        </div>
+    )
+}
